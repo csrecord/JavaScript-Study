bfb5113ab41e5045a91385b300f681e1f7f5c22e
chenhengfei <821132332@qq.com>
2022-08-02 11:00:12 +0800

    JS first commit

---
+++ 01.JS学习/myJavaScript.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "我的第一个 JavaScript 函数";
+
+}
+function myDiv() {
+    document.getElementById("myDiv").innerHTML = "我的第二个 JavaScript 函数";
+}
